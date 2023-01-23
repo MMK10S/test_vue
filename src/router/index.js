28df037d498e77e920540e1d7f1684edb1ae4e23
Maxym Kuryloa <maxym.mykolayovych@gmail.com>
2023-01-23 22:11:26 +0200

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      redirect: () => '/some',
+      children:[
+        {
+          path: 'some',
+          name: 'some',
+          // route level code-splitting
+          // this generates a separate chunk (About.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import('@/views/SomePage.vue'),
+        }
+      ]
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('@/views/AboutView.vue'),
+    },
+    
+  ]
+})
+
+export default router
---
+++ src/views/SomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="some">
+    <h1>Some page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .some {
+    min-height: 100vh;
+    display: flexbox;
+    align-items: center;
+  }
+}
+</style>
